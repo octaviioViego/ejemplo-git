52a79580889e9b17a8bd4f6da3fc468ed4ef2cfd
octaviioViego <octavio.arroyo@estudiante.uacm.edu.mx>
2025-05-27 20:03:50 -0600

    Revert "Se agrego el suma actualizado"
    This reverts commit 43de0db4dafd9639bbf40e5d849d5c5015a58d53.

--- holaMundo.c
+++ holaMundo.c
@@ -1,15 +1,5 @@
 #include <stdio.h>
 
-int factorial(int n){
-	if(n==0) return 1;
-	return n  + factorial(n-1);
-}
-
-int suma(int n){
-	if(n==0) return 0;
-	return n + suma(n-1);
-}
-
 int main(){
 	printf("Hola mundo");
 }
